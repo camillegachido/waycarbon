36d53c20d7778c582942d85ac83dace284d835ea
Camille Gachido <camillegachido70@gmail.com>
2024-08-02 19:39:57 -0300

    feat: add theme provider

---
+++ src/common/constants/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  primary: {
+    main: 'rgba(3, 169, 244, 1)',
+    dark: 'rgba(1, 125, 181, 1)',
+  },
+  secondary: {
+    main: 'rgba(255, 155, 82, 1)',
+  },
+  text: {
+    primary: 'rgba(0, 0, 0, 1)',
+    secondary: 'rgba(147, 147, 147, 1)',
+    terciary: 'rgba(138, 138, 138, 1)',
+  },
+};
