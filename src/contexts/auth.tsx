f6d839d46c0ca4961e5a84e58f35e1da83270f48
Camille Gachido <camillegachido70@gmail.com>
2024-08-03 11:49:54 -0300

    feat: auth context

---
+++ src/contexts/auth.tsx
@@ -0,0 +1,23 @@
+import { createContext, PropsWithChildren } from 'react';
+import { User } from '../common/types/user';
+import { users } from '../services/data/users';
+
+export interface AuthContext {
+  user: User;
+}
+
+const loggedUser = users[0];
+
+export const AuthContext = createContext<AuthContext>({
+  user: loggedUser,
+});
+
+const AuthProvider = ({ children }: PropsWithChildren) => {
+  const user = loggedUser;
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
