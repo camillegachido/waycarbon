778fc85201b2f6706dc6b6de4b110cc2404dbc06
Camille Gachido <camillegachido70@gmail.com>
2024-08-03 00:39:32 -0300

    feat: add user component

---
+++ src/components/user/index.spec.tsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+import { User } from '.';
+import { mockThemeProvider } from '../../common/utils/test';
+
+describe('User Component', () => {
+  const mockUser = {
+    username: 'testuser',
+    memberSince: 'January 2020',
+    friendIds: [],
+    posts: [],
+    id: 1,
+  };
+
+  it('should render user information correctly', () => {
+    const { getByText, getByAltText } = render(
+      mockThemeProvider(<User user={mockUser} />)
+    );
+
+    expect(getByText(mockUser.username)).toBeInTheDocument();
+
+    expect(getByText(mockUser.memberSince)).toBeInTheDocument();
+
+    expect(getByAltText(mockUser.username + ' avatar')).toBeInTheDocument();
+  });
+});
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -16,12 +16,10 @@ export const GlobalStyle = createGlobalStyle`
     -moz-osx-font-smoothing: grayscale;
   }
 
-  body, html {
+  body, html, * {
     margin: 0;
     padding: 0;
-  }
-
-  *{
     font-family: "Roboto", sans-serif;
   }
+
 `;
