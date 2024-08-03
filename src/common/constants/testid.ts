f98c77ec4df41441cff667518e5f1ff4baa9a6a5
Camille Gachido <camillegachido70@gmail.com>
2024-08-03 00:13:25 -0300

    feat: add button component

--- src/common/constants/testid.ts
+++ src/common/constants/testid.ts
@@ -2,4 +2,9 @@ export const componentsTestId = {
   modal: {
     background: 'modal-background',
   },
+  button: {
+    default: 'button-default',
+    outlined: 'button-outlined',
+    text: 'button-text',
+  },
 };
---
+++ src/components/button/index.tsx
@@ -0,0 +1,21 @@
+import { Default, Outlined, Text } from './index.styled';
+import { componentsTestId } from '../../common/constants/testid';
+import { ButtonHTMLAttributes } from 'react';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'default' | 'text' | 'outlined';
+  small?: boolean;
+  color?: string;
+}
+
+export const Button = ({ variant = 'default', ...props }: Props) => {
+  if (variant === 'text')
+    return <Text data-testid={componentsTestId.button.text} {...props} />;
+
+  if (variant === 'outlined')
+    return (
+      <Outlined data-testid={componentsTestId.button.outlined} {...props} />
+    );
+
+  return <Default data-testid={componentsTestId.button.default} {...props} />;
+};
