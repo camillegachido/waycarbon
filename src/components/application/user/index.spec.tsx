fe5910b88344d2bd512ea13bc079fe1adb16e5ac
Camille Gachido <camillegachido70@gmail.com>
2024-08-04 17:58:30 -0300

    test: refactor user compnent test to use formatDate function

--- src/components/application/user/index.spec.tsx
+++ src/components/application/user/index.spec.tsx
@@ -1,11 +1,13 @@
 import { render } from '@testing-library/react';
 import { User } from '.';
 import { mockThemeProvider } from '../../../common/utils/test';
+import { formatDate } from '../../../common/utils';
 
 describe('User Component', () => {
   const mockUser = {
     username: 'testuser',
     id: 1,
+    avatar_url: '',
   };
 
   const time = '2019-02-20T13:22Z';
@@ -17,7 +19,7 @@ describe('User Component', () => {
 
     expect(getByText(mockUser.username)).toBeInTheDocument();
 
-    expect(getByText(time)).toBeInTheDocument();
+    expect(getByText(formatDate(time))).toBeInTheDocument();
 
     expect(getByAltText(mockUser.username + ' avatar')).toBeInTheDocument();
   });
