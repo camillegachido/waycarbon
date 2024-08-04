07c9f74bbfa4007d1f3caf4f1ad57051d8738739
Camille Gachido <camillegachido70@gmail.com>
2024-08-04 14:23:18 -0300

    test> add page blog comment e2e test

--- cypress/e2e/post.cy.ts
+++ cypress/e2e/post.cy.ts
@@ -1,5 +1,61 @@
+import { componentsTestId } from '../../src/common/constants/testid';
+
 describe('Post page', () => {
-  it('should display the post page', () => {
+  it('should reply comments', () => {
     cy.visit('/');
+    cy.get(`[data-testid=${componentsTestId.loader.container}]`).should(
+      'exist'
+    );
+    cy.get(`[data-testid=${componentsTestId.loader.container}]`).should(
+      'not.exist'
+    );
+
+    cy.get(`[data-testid=${componentsTestId.comment.container}]`).should(
+      'have.length',
+      5
+    );
+
+    cy.get(
+      `[data-testid=${componentsTestId.comment.container}]:first [data-testid=${componentsTestId.comment.replybutton}]`
+    ).click();
+
+    cy.get(`[data-testid=${componentsTestId.comment.reply}]`).should('exist');
+
+    const typedText = 'Reply text';
+    cy.get(`[data-testid=${componentsTestId.comment.reply}] textarea`).type(
+      typedText
+    );
+    cy.get(
+      `[data-testid=${componentsTestId.comment.reply}] [data-testid=${componentsTestId.button.default}]`
+    ).click();
+
+    cy.get(`[data-testid=${componentsTestId.comment.reply}]`).should(
+      'not.exist'
+    );
+
+    const addedComment = `[data-testid=${componentsTestId.comment.container}]:first [data-testid=comment-container]`;
+
+    cy.get(addedComment).should('contains.text', typedText);
+
+    cy.get(
+      `${addedComment} [data-testid=${componentsTestId.comment.replybutton}]`
+    ).click();
+
+    cy.get(`[data-testid=${componentsTestId.comment.reply}]`).should('exist');
+    const typedTextReply = 'Reply to reply text';
+    cy.get(`[data-testid=${componentsTestId.comment.reply}] textarea`).type(
+      typedTextReply
+    );
+    cy.get(
+      `[data-testid=${componentsTestId.comment.reply}] [data-testid=${componentsTestId.button.default}]`
+    ).click();
+
+    cy.get(`[data-testid=${componentsTestId.comment.reply}]`).should(
+      'not.exist'
+    );
+
+    cy.get(
+      `${addedComment} [data-testid=${componentsTestId.comment.container}]`
+    ).should('contains.text', typedTextReply);
   });
 });
