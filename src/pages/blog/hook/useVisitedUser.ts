44d90574a106a45cb411ff0f57b742af0f5de077
Camille Gachido <camillegachido70@gmail.com>
2024-08-04 15:24:27 -0300

    feat: return only common friends on useVisitedUser hook

--- src/pages/blog/hook/useVisitedUser.ts
+++ src/pages/blog/hook/useVisitedUser.ts
@@ -1,17 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { GetUserUseCase } from '../../../usecase';
 import { UserWithFriends } from '../../../common/types/user';
+import { AuthContext } from '../../../contexts/auth';
 
 export const useVisitedUser = (id: number) => {
   const [isLoading, setIsLoading] = useState(false);
   const [user, setUser] = useState<UserWithFriends | null>(null);
+  const { user: loggedUser } = useContext(AuthContext);
 
   useEffect(() => {
     setIsLoading(true);
     new GetUserUseCase()
       .execute(id)
       .then((data) => {
-        setUser(data);
+        setUser({
+          ...data,
+          friends: data.friends.filter((friend) => friend.id !== loggedUser.id),
+        });
       })
       .finally(() => {
         setIsLoading(false);
