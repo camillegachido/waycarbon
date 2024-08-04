f669e50fd8ebecc7eca4bb289726fd0033820549
Camille Gachido <camillegachido70@gmail.com>
2024-08-04 15:12:22 -0300

    chore: add loader to common export

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -3,5 +3,6 @@ import { Header } from './header';
 import { Button } from './button';
 import { Modal } from './modal';
 import { Tabs } from './tabs';
+import { Loader } from './loader';
 
-export { Layout, Header, Button, Modal, Tabs };
+export { Layout, Header, Button, Modal, Tabs, Loader };
