329331e4c5f0955c91f1064c872ca37a23b70cda
Camille Gachido <camillegachido70@gmail.com>
2024-08-04 18:05:47 -0300

    feat: add loading avatar

--- src/components/application/user/index.tsx
+++ src/components/application/user/index.tsx
@@ -1,6 +1,7 @@
 import { GenericUser } from '../../../common/types/user';
 import { formatDate } from '../../../common/utils';
 import { Container, Image, Name, Date } from './index.styled';
+import LoadAvatar from '../../../assets/images/load-avatar.jpg';
 
 interface Props {
   user: GenericUser;
@@ -11,9 +12,10 @@ interface Props {
 export const User = ({ user, time, onClick }: Props) => {
   const isClickable = !!onClick;
 
+  const url = user.avatar_url !== '' ? user.avatar_url : LoadAvatar;
   return (
     <Container isClickable={isClickable} onClick={onClick}>
-      <Image src={user.avatar_url} alt={user.username + ' avatar'} />
+      <Image src={url} alt={user.username + ' avatar'} />
       <div>
         <Name>{user.username}</Name>
         <Date>{formatDate(time)}</Date>
