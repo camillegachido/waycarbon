a43f2ecb51fd16660f977b372ce48a7dc90690cf
Camille Gachido <camillegachido70@gmail.com>
2024-08-04 17:12:35 -0300

    feat: remove App.spec

--- src/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import App from './App';
-import { render, screen } from '@testing-library/react';
-
-describe('App', () => {
-  test('renders app page', async () => {
-    render(<App />);
-    expect(screen.getByText('Vite + React')).toBeInTheDocument();
-  });
-});
