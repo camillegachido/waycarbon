cf05413e44a5099cea06087efeba7632fea6a1c7
Camille Gachido <camillegachido70@gmail.com>
2024-08-04 15:02:58 -0300

    feat: add selectedUser on usePost hook

--- src/pages/blog/hook/usePost.ts
+++ src/pages/blog/hook/usePost.ts
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 import { NestedPost } from '../../../common/types/posts';
 import { GetPostUseCase } from '../../../usecase';
+import { GenericUser } from '../../../common/types/user';
 
 export const usePost = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [post, setPost] = useState<NestedPost | null>(null);
+  const [selectedUser, setSelectedUser] = useState<GenericUser | null>(null);
 
   useEffect(() => {
     setIsLoading(true);
@@ -18,5 +20,13 @@ export const usePost = () => {
       });
   }, []);
 
-  return { isLoading, post };
+  const onAuthorClick = (author: GenericUser) => {
+    setSelectedUser(author);
+  };
+
+  const onDeselectUser = () => {
+    setSelectedUser(null);
+  };
+
+  return { isLoading, post, onAuthorClick, selectedUser, onDeselectUser };
 };
